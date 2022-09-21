521afe0ea67aebe1b6860f324ffd81c799a5d482
Futó Csaba Arnold <61312438+Csabi1998@users.noreply.github.com>
2022-09-21 14:32:44 +0200

    #4 Event binding

--- cmp-databinding/src/app/app.component.ts
+++ cmp-databinding/src/app/app.component.ts
@@ -9,4 +9,23 @@ export class AppComponent {
   serverElements = [
     { type: 'server', name: 'TesztServer', content: 'Just a teszt!' },
   ];
+
+  onServerAdded(serverData: { serverName: string; serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent,
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {
+    serverName: string;
+    serverContent: string;
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent,
+    });
+  }
 }
