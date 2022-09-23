f5c73e3c3aaa3e40b42570405bd93655dcad92fa
Futó Csaba Arnold <61312438+Csabi1998@users.noreply.github.com>
2022-09-23 18:11:26 +0200

    #4 Directives Deep dive, on Course project to

--- Shopping-Recipe/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ Shopping-Recipe/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recepie } from '../../recepie.model';
 
 @Component({
   selector: 'app-recepie-item',
   templateUrl: './recepie-item.component.html',
-  styleUrls: ['./recepie-item.component.css']
+  styleUrls: ['./recepie-item.component.css'],
 })
 export class RecepieItemComponent implements OnInit {
+  @Input() recipe: Recepie;
+  @Output() recipeSelected = new EventEmitter<void>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSelected() {
+    this.recipeSelected.emit();
   }
-
 }
--- cmp-databinding/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -15,24 +22,26 @@ export class CockpitComponent implements OnInit {
     serverContent: string;
   }>();
 
-  newServerName = '';
-  newServerContent = '';
+  /*   newServerName = ''; */
+  /*   newServerContent = ''; */
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
